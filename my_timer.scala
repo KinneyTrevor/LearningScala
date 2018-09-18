831437fb083475c3109b499ac3b1bca61b38e4d9
Trevor Kinney <trevorkinney@hotmail.com>
2018-09-18 09:14:38 -0600

    Main repl working. Added regex to pattern match acceptable user input

--- my_timer.scala
+++ my_timer.scala
@@ -10,17 +10,52 @@ class MyTimer{
   // Prompt to press any key to resume
   // End application when timer runs out
 
-  def MyTimer(){
+  val running : Boolean = true
+
+  def main(args: Array[String]): Unit = {
     println("Welcome to my timer")
-    val input = readLine("Please enter a time input: \n")
-    println(f"Your string is: $input")
-    check_user_input(input)
+    println("Please enter a time input in any of the following formats")
+    println("Number Unit -- 1M, 1 Minute, 2 H, 2Hours, 78s, 78 seconds")
+
+    while(running){
+      val input = GetInput()
+      if(CheckUserInput(input : String)){
+        println("Valid input")
+
+      }
+    }
+  }
+  
+  def GetInput(): String = {
+
+    val input = readLine()
+    return input
+
   }
 
-  def CheckUserInput(val userInput : String){
-    println(f"Value passed is: $userInput" )
+  def CheckUserInput(userInput : String): Boolean = {
+
+    // Delete me
+    println(f"Value passed is: $userInput")
+
+    // Use raw to process regex without having to worry about escaping special chars
+    // This breaks on 45mhs
+    val AcceptablePattern = raw"(\d)+\s?([MmHhSs]+)".r
+
+    AcceptablePattern.findFirstMatchIn(userInput) match {
+      case Some(_) => println("It's a match pal")
+      case None => println("No can do")
+    }
+
+    return true
 
   }
+
+  def ParseUserInput(): Unit={
+
+  }
+
+  main(Array())
 
 
 }
